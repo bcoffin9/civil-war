ddb8751150dc837f464a58fa53ba88ffb16e9f40
Billy Coffin <coffinw9@gmail.com>
2019-05-13 21:58:26 -0400

    one minor change. Fingers crossed

--- db/migrate/20190510014300_create_scene.rb
+++ db/migrate/20190510014300_create_scene.rb
@@ -2,7 +2,7 @@ class CreateScene < ActiveRecord::Migration[5.2]
   def change
     create_table :scenes do |t|
       t.string "name"
-      t.string "type_of_scene"
+      t.string "type"
       t.text "description"
       t.references :battle
     end
